09c6c91a93ee303d8b01fc2508d79dbae949b1cf
JCharly95 <juancarloscharly@live.com.mx>
2023-05-16 19:48:14 -0600

    Alarma
    En este commit se creó la interfaz con funcionalidad de la alarma sincronizado con la entidad en firebase y realizando cambios en tiempo real hacia y desde la App.

--- app/src/main/java/com/ardusec/ardu_security/AlarmActivity.kt
+++ app/src/main/java/com/ardusec/ardu_security/AlarmActivity.kt
@@ -4,12 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
-import androidx.core.view.isGone
 import com.google.android.material.switchmaterial.SwitchMaterial
 import com.google.firebase.database.*
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
-import com.google.gson.Gson
 
 class AlarmActivity : AppCompatActivity() {
     private lateinit var lblSta: TextView
@@ -37,19 +35,23 @@ class AlarmActivity : AppCompatActivity() {
         // Inicializando instancia hacia el nodo raiz de la BD
         database = Firebase.database
 
+        // Estableciendo el estado del switch acorde a la info de firebase
+        setSwitch()
+    }
+
+    private fun setSwitch(){
         // Establecer el estado del switch acorde al valor de la entidad en firebase
-        ref = database.getReference("Alarma")
+        ref = database.getReference("Alarma").child("estado")
         ref.addValueEventListener(object: ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot){
-                val estado = dataSnapshot.children
-                Log.w("FirebaseResult", "Estado de la alarma en firebase: $estado")
-                /*if(estado){
+                val estado = dataSnapshot.value
+                if(estado as Boolean){
                     lblSta.text = resources.getString(R.string.alEstaAct)
                     swAlarma.isChecked = true
                 }else{
                     lblSta.text = resources.getString(R.string.alEstaDesa)
                     swAlarma.isChecked = false
-                }*/
+                }
             }
             override fun onCancelled(databaseError: DatabaseError) {
                 Log.w("FirebaseError", "Error: No se pudieron obtener o no se pudieron actualizar los valores solicitados", databaseError.toException())
@@ -58,8 +60,14 @@ class AlarmActivity : AppCompatActivity() {
     }
 
     private fun addListeners(){
-        swAlarma.setOnClickListener {
-            swAlarma.isChecked = !swAlarma.isChecked
+        swAlarma.setOnCheckedChangeListener{ _, isChecked ->
+            if (isChecked){
+                swAlarma.isChecked = true
+                database.getReference("Alarma").child("estado").setValue(true)
+            }else{
+                swAlarma.isChecked = false
+                database.getReference("Alarma").child("estado").setValue(false)
+            }
         }
     }
 }
